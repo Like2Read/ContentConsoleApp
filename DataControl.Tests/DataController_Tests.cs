cc2c295a4a79d644f0d29d413230d907695c91cf
Budanov Aleksandr <aleksandr.budanov@volvo.com>
2018-06-26 07:53:42 +0200

    Tests for DataController added

---
+++ DataControl.Tests/DataController_Tests.cs
@@ -0,0 +1,171 @@
+﻿using System.Linq;
+using DataControl.Mock;
+using DataControl.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace DataControl.Tests
+{
+    [TestClass]
+    public class DataController_Tests
+    {
+        private readonly DataController _bad_horrible_controller;
+        private readonly string[] _initial_bad_words = new string[] { "bad", "horrible" };
+        private readonly string _example_text = @"The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
+
+        public DataController_Tests()
+        {
+            var mock_repository = new Mock<IWordsRepository>();
+            mock_repository.Setup(m => m.GetAllWords()).Returns(_initial_bad_words.ToArray());
+            _bad_horrible_controller = new DataController(mock_repository.Object);
+        }
+
+        [TestMethod]
+        public void ScanTextForWords_ReturnsScanResult()
+        {
+            var result = _bad_horrible_controller.ScanTextForWords("");
+
+            Assert.IsTrue(result is ScanResult);
+        }
+
+        [TestMethod]
+        public void ScanTextForWords_Find2BadWordsInTheTextFromTheStory()
+        {
+            var result = _bad_horrible_controller.ScanTextForWords(_example_text) as ScanResult;
+            Assert.AreEqual(result.Count, 2);
+        }
+
+        [TestMethod]
+        public void ScanTextForWords_Find3BadWordsOneIsDuplicated()
+        {
+            var result = _bad_horrible_controller.ScanTextForWords("It was a bad day. Not just bad, it was horrible.") as ScanResult;
+            Assert.AreEqual(result.Count, 3);
+        }
+
+        [TestMethod]
+        public void ScanTextForWords_Find2BadWordsInTheTextFromTheStoryUpperCaseIgnored()
+        {
+            var result = _bad_horrible_controller.ScanTextForWords(_example_text.ToUpperInvariant()) as ScanResult;
+            Assert.AreEqual(result.Count, 2);
+        }
+
+        [TestMethod]
+        public void ScanTextForWords_Find1BadWordsInTheText()
+        {
+            var result = _bad_horrible_controller.ScanTextForWords("It was a bad day") as ScanResult;
+            Assert.AreEqual(result.Count, 1);
+        }
+
+        [TestMethod]
+        public void ScanTextForWords_Find0BadWordsInTheEmptyText()
+        {
+            var result = _bad_horrible_controller.ScanTextForWords("") as ScanResult;
+            Assert.AreEqual(result.Count, 0);
+        }
+
+        [TestMethod]
+        public void ScanTextForWords_Find1BadWordInTheTextWithQuestionSign()
+        {
+            var result = _bad_horrible_controller.ScanTextForWords("Was this day bad?") as ScanResult;
+            Assert.AreEqual(result.Count, 1);
+        }
+
+
+        [TestMethod]
+        public void GetAllWords_ResultIsTheSameAsInitialWords()
+        {
+            var result = _bad_horrible_controller.GetAllWords();
+            CollectionAssert.AreEqual(_initial_bad_words, result.ToArray());
+        }
+
+        [TestMethod]
+        public void SetWords_ReturnsUpdateResult()
+        {
+            var mock_repository = new Mock<IWordsRepository>();
+            mock_repository.Setup(m => m.GetAllWords()).Returns(_initial_bad_words.ToArray());
+            var controller = new DataController(mock_repository.Object);
+            var result = controller.SetWords(new string[] { "terrible" });
+
+            Assert.IsTrue(result is UpdateResult);
+        }
+
+        [TestMethod]
+        public void SetWords_ReturnsTheSameAsNewWords()
+        {
+            var newWords = new string[] { "terrible" };
+            var mock_repository = new MoqWordsRepository(new string[] { "bad", "horrible" });
+            var controller = new DataController(mock_repository);
+            controller.SetWords(newWords);
+
+            var words = controller.GetAllWords();
+            CollectionAssert.AreEqual(words.ToArray(), newWords);
+        }
+
+        [TestMethod]
+        public void SetWords_CheckScanResultsBeforeAndAfter()
+        {
+            var newWords = new string[] { "terrible" };
+            var mock_repository = new MoqWordsRepository(new string[] { "bad", "horrible" });
+            var controller = new DataController(mock_repository);
+
+            var resultBefore = controller.ScanTextForWords(_example_text) as ScanResult;
+            controller.SetWords(newWords);
+            var resultAfter = controller.ScanTextForWords(_example_text) as ScanResult;
+
+            Assert.AreNotEqual(resultBefore.Count, resultAfter.Count);
+        }
+
+        [TestMethod]
+        public void SetWords_TryToSetNull()
+        {
+            var mock_repository = new Mock<IWordsRepository>();
+            mock_repository.Setup(m => m.GetAllWords()).Returns(_initial_bad_words.ToArray());
+            var controller = new DataController(mock_repository.Object);
+            var result = controller.SetWords(null) as UpdateResult;
+
+            Assert.AreEqual(result.Message, Messages.WordsAreNull);
+        }
+
+        [TestMethod]
+        public void SetWords_TryToSetArrayWithNullAndEmptyWords()
+        {
+            var mock_repository = new MoqWordsRepository(new string[] { "bad", "horrible" });
+            var controller = new DataController(mock_repository);
+
+            var result = controller.SetWords(new string[] { null, "", "terrible", "worse" }) as UpdateResult;
+            var newWords = controller.GetAllWords();
+
+            CollectionAssert.AreEqual(new string[] { "terrible", "worse" }, newWords.OrderBy(x => x).ToArray());
+        }
+
+        [TestMethod]
+        public void HideWords_BadWordsInStoryTextAreHidden()
+        {
+            _bad_horrible_controller.TryHideWords(_example_text, out string edited_text);
+            Assert.AreEqual("The weather in Manchester in winter is b#d. It rains all the time - it must be h######e for people visiting.",
+                edited_text);
+        }
+
+        [TestMethod]
+        public void HideWords_WordsSimilarToBadWordsStayVisible()
+        {
+            var text = "Badler was not that bad";
+            _bad_horrible_controller.TryHideWords(text, out string edited_text);
+            Assert.AreEqual("Badler was not that b#d",
+                edited_text);
+        }
+
+        [TestMethod]
+        public void HideWords_WordsWithLengthLessThan3ReplacedWithHashes()
+        {
+            var mock_repository = new Mock<IWordsRepository>();
+            mock_repository.Setup(m => m.GetAllWords()).Returns(new string[] { "no", "x" });
+            var controller = new DataController(mock_repository.Object);
+
+            var text = "The first letter is no X - that's for sure";
+            controller.TryHideWords(text, out string edited_text);
+            Assert.AreEqual("The first letter is ## # - that's for sure",
+                edited_text);
+        }
+    }
+}
---
+++ DataControl.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataControl.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("DataControl.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("76f96fc5-719e-4ee0-8eaa-2e85e35f20b9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

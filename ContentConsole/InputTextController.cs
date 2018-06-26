a667c5a5df364031bda09d8cf8a34e9c42ec0821
Budanov Aleksandr <aleksandr.budanov@volvo.com>
2018-06-26 10:02:52 +0200

    Comments removed

--- ContentConsole/InputTextController.cs
+++ ContentConsole/InputTextController.cs
@@ -37,36 +37,6 @@ namespace ContentConsole
 
             return new CommandResultWithText(readerFunction(path));
         }
-/*
-        public static ICommandResult ReadWordsFromFile(string[] args)
-        {
-            if (args == null || args.Length < 2)
-            {
-                return new CommandResult(false, "No file with words was provided");
-            }
-            var path = Path.Combine("Files", args[1]);
-
-            if (!TryReadWords(path, out string[] newWords))
-            {
-                return new CommandResult(false, $"File {args[1]} was not found in Files folder");
-            }
-
-            return new CommandResultWithText(string.Join(" ", newWords));
-        }
-
-        public static ICommandResult ReadTextFromFile(string[] args)
-        {
-            if (args.Length < 2)
-            {
-                return new CommandResult(false, "No file with text was provided");
-            }
-            var path = Path.Combine("Files", args[1]);
-            if (!File.Exists(path))
-                return new CommandResult(false, $"File {args[1]} was not found in Files folder");
-
-            return new CommandResultWithText(File.ReadAllText(path));
-        }
-*/
 
     }
 }
